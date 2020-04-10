9f9f6885748ee128ca0e06aef95cfb7ca29b16c2
DangQuy <ngoc_quy@allgrow-labo.jp>
2020-04-10 14:28:45 +0700

    addd

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,26 +1,24 @@
-$(document).ready( ()=>{
-    // this will get the full URL at the address bar
-    var url = window.location.href; 
-    // passes on every "a" tag 
-    $("#c-gnav a").each(function() {
-        if(url == (this.href)) {
-            $(this).closest("li").addClass("is_active");
+$('document').ready(function() {
+    $(".c-scroll").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    })
+    $(window).on('scroll', function() {
+        if($(this).scrollTop() > 200) {
+            $(".c-scroll").fadeIn();
+        } else {
+            $(".c-scroll").fadeOut();
         }
     });
-    
-    // slider
-    var myIndex = 0;
-    carousel();
 
-    function carousel() {
-        var i;
-        var x = document.getElementsByClassName("c-slider__banner");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";  
+    $(".c-menu__icon").click(function() {
+        console.log(this);
+        var existClassHidden = $(".close").hasClass("__hidden");
+        if (existClassHidden) {
+            $(this).children().removeClass("__hidden");
+            $(".open").addClass("__hidden");
+        } else {
+            $('.open').removeClass("__hidden");
+            $(".close").addClass("__hidden");
         }
-        myIndex++;
-        if (myIndex > x.length) {myIndex = 1}    
-        x[myIndex-1].style.display = "block";  
-        setTimeout(carousel, 2000); // Change image every 2 seconds
-    }
-})
+    })
+});
